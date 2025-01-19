762d08f5f86f84a99e2a0b77d595a2d591e633b4
Jodking <tripathihitesh580@gmail.com>
2025-01-19 23:10:22 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -118,18 +118,15 @@ export const userUpdate = async (req, res) => {
 
     // Handle profile picture update
     if (req.file) {
-      const b64 = Buffer.from(req.file.buffer).toString("base64");
-      let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+      const { destination, filename } = req.file;
+      const path = `${destination}${filename}`;
+      const imgurl = await uploadImage(path);
 
-      const uploadResponse = await cloudinary.uploader.upload(dataURI, {
-        resource_type: "auto",
-      });
-
-      if (!uploadResponse.secure_url) {
+      if (!imgurl) {
         return res.status(400).json({ message: "Error uploading image" });
       }
 
-      updateData.profilePic = uploadResponse.secure_url;
+      updateData.profilePic = imgurl;
     }
 
     // Handle username update
